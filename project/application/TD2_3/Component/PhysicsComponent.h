d474d564859ab8109e14d28d1b4f7d4236b5d787
GoShIYO <k024g1025@g.neec.ac.jp>
2026-01-26 15:59:15 +0900

    敵仮作成

--- project/application/TD2_3/Component/PhysicsComponent.h
+++ project/application/TD2_3/Component/PhysicsComponent.h
@@ -5,6 +5,7 @@ struct PhysicsComponent
 { 
     NoEngine::Vector3 velocity;
 	float coefficient = 0.9f;				//反発係数
+	float friction = 0.7f;					//摩擦係数
 	bool useGravity = true;					//重力を使うか
 
 	static constexpr float kGravity = 6.0f;	//重力
