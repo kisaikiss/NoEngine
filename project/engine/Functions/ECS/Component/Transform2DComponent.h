e89e1a88f3f8244cf73c406a52ddd77ce1ef10c2
kobayashi masato <k024g1031@g.neec.ac.jp>
2026-02-20 14:14:43 +0900

    webhookテスト

--- project/engine/Functions/ECS/Component/Transform2DComponent.h
+++ project/engine/Functions/ECS/Component/Transform2DComponent.h
@@ -5,7 +5,6 @@ namespace NoEngine {
 namespace Component {
 struct Transform2DComponent : public Transform2D {
 	Transform2DComponent() = default;
-
 	Transform2DComponent(const Math::Vector2& position, float rotation, const Math::Vector2& scale) : Transform2D(position, rotation, scale) {}
 };
 }
