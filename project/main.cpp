93c2f3c4d70a93994e1029b8b3a8956cee55f7bb
kobayashi masato <k024g1031@g.neec.ac.jp>
2026-02-20 14:46:22 +0900

    4ヶ月制作用アプリケーションを追加しました。

--- project/main.cpp
+++ project/main.cpp
@@ -1,4 +1,5 @@
 #include "engine/Runtime/GameCore.h"
 #include "application/TestApp/TestApp.h"
+#include "application/TD3/OneSide.h"
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return NoEngine::GameCore::RunApplication(std::make_unique<TestApp>()); }
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return NoEngine::GameCore::RunApplication(std::make_unique<OneSide>()); }
