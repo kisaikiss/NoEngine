9557370890b02b7e1c5206bc9463314d6da72e97
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2026-03-05 20:21:51 +0900

    カメラの更新を反映

--- project/application/TD3/Scene/SampleScene.cpp
+++ project/application/TD3/Scene/SampleScene.cpp
@@ -8,6 +8,13 @@ void SampleScene::Setup() {
 	registry.AddComponent<No::CameraComponent>(camera);
 	auto* cameraTransform = registry.AddComponent<No::TransformComponent>(camera);
 	cameraTransform->translate.z = -5.f;
+
+
+	auto light = registry.GenerateEntity();
+	auto* dir = registry.AddComponent<No::DirectionalLightComponent>(light);
+	dir->color = { 1.f,1.f,1.f,1.f };
+	dir->direction = { 0.f,-1.f,0.f };
+	dir->intensity = 1.f;
 }
 
 void SampleScene::NotSystemUpdate() {
--- project/main.cpp
+++ project/main.cpp
@@ -1,4 +1,5 @@
 #include "engine/Runtime/GameCore.h"
 #include "application/TD3/OneSide.h"
+#include "application/TestApp/TestApp.h"
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return NoEngine::GameCore::RunApplication(std::make_unique<TestApp>()); }
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return NoEngine::GameCore::RunApplication(std::make_unique<OneSide>()); }
