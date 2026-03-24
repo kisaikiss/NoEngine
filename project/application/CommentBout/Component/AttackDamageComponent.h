38918a32f2c9a819dc1c06b9bee820a3e8fb1a9a
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2026-03-24 21:32:20 +0900

    敵撃破判定

--- project/application/CommentBout/Component/AttackDamageComponent.h
+++ project/application/CommentBout/Component/AttackDamageComponent.h
@@ -1,5 +1,9 @@
 #pragma once
+#include "engine/NoEngine.h"
+#include <vector>
 
 struct AttackDamageComponent {
 	int damage = 10;
+	std::vector<No::Entity> hitEnemies;
+	bool isFirstFrameConsumed = false;
 };
