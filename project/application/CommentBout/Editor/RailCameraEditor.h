51debdc371c9a7394b07542c87d215412ac7e311
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2026-03-29 01:07:38 +0900

    RailcameraEditorタブ付きのコードを消し忘れていた

--- project/application/CommentBout/Editor/RailCameraEditor.h
+++ project/application/CommentBout/Editor/RailCameraEditor.h
@@ -7,10 +7,6 @@ class RailCameraEditor {
 public:
 	using ResetEventRuntimeFn = void(*)(RailCameraComponent&);
 
-	/// フローティングウィンドウ版 (ImGui::Begin/End 付き)
-	void DrawRailCameraImGui(No::Registry* registry, No::Entity railCameraEntity, ResetEventRuntimeFn resetEventRuntime);
-	void DrawRailEditorImGui(No::Registry* registry, No::Entity railCameraEntity);
-
 	/// タブ内インライン版 (ImGui::Begin/End なし。タブアイテム内から呼ぶ)
 	void DrawRailCameraContent(No::Registry* registry, No::Entity railCameraEntity, ResetEventRuntimeFn resetEventRuntime);
 	void DrawRailEditorContent(No::Registry* registry, No::Entity railCameraEntity);
