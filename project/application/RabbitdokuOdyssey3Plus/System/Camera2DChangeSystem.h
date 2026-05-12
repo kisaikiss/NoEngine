bacbcb1c3448f386739390a820c49364d16d4763
kobayashi masato <k024g1031@g.neec.ac.jp>
2026-05-12 16:37:54 +0900

    fix: TD3のDebugCamera切り替えをいつでも出来るように修正

--- project/application/RabbitdokuOdyssey3Plus/System/Camera2DChangeSystem.h
+++ project/application/RabbitdokuOdyssey3Plus/System/Camera2DChangeSystem.h
@@ -3,6 +3,7 @@
 
 class Camera2DChangeSystem : public No::ISystem {
 public:
+	Camera2DChangeSystem(){ SetStopInGameStop(false); SetStopInPause(false); }
 	void Update(No::Registry& registry, float deltaTime) override;
 };
 
